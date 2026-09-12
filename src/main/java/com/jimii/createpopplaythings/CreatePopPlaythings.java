3f8928049105e63ab36f4916b0aedefaa9ea7e2b
gimiibrahim00-ctrl <gimiibrahim00@gmail.com>
2026-09-13 09:21:57 +1000

    build: add src/main/java/com/jimii/createpopplaythings/CreatePopPlaythings.java

---
+++ src/main/java/com/jimii/createpopplaythings/CreatePopPlaythings.java
@@ -0,0 +1,11 @@
+package com.jimii.createpopplaythings;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(CreatePopPlaythings.MOD_ID)
+public final class CreatePopPlaythings {
+    public static final String MOD_ID = "create_pop_playthings";
+
+    public CreatePopPlaythings() {
+    }
+}
