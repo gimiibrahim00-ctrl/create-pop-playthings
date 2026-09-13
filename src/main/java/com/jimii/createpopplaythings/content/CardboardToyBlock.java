fb36d1645444923079dca234c90bf4c8f581f30d
gimiibrahim00-ctrl <gimiibrahim00@gmail.com>
2026-09-13 12:34:49 +1000

    feat: add CardboardToyBlock.java

---
+++ src/main/java/com/jimii/createpopplaythings/content/CardboardToyBlock.java
@@ -0,0 +1,7 @@
+package com.jimii.createpopplaythings.content;
+
+import net.minecraft.world.level.block.state.BlockBehaviour;
+
+public final class CardboardToyBlock extends PopToyBlock {
+    public CardboardToyBlock(BlockBehaviour.Properties properties) { super(properties); }
+}
