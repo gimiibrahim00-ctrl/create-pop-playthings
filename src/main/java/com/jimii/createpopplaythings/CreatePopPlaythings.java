a3edf7d8f49ce2f518187120405355a971335c6c
gimiibrahim00-ctrl <gimiibrahim00@gmail.com>
2026-09-13 12:45:36 +1000

    feat: wire CreatePopPlaythings.java

--- src/main/java/com/jimii/createpopplaythings/CreatePopPlaythings.java
+++ src/main/java/com/jimii/createpopplaythings/CreatePopPlaythings.java
@@ -1,11 +1,19 @@
 package com.jimii.createpopplaythings;
 
 import net.neoforged.fml.common.Mod;
+import net.neoforged.bus.api.IEventBus;
+import com.jimii.createpopplaythings.registry.ModBlocks;
+import com.jimii.createpopplaythings.registry.ModEntities;
+import com.jimii.createpopplaythings.registry.ModItems;
 
 @Mod(CreatePopPlaythings.MOD_ID)
 public final class CreatePopPlaythings {
     public static final String MOD_ID = "create_pop_playthings";
 
-    public CreatePopPlaythings() {
+    public CreatePopPlaythings(IEventBus modBus) {
+        ModBlocks.BLOCKS.register(modBus);
+        ModItems.ITEMS.register(modBus);
+        ModEntities.ENTITIES.register(modBus);
+        modBus.addListener(ModEntities::createAttributes);
     }
 }
